4e6a552d693664b8374d6e429778121729fa67bf
Giovanni Branco <giovannipbranco1@gmail.com>
2021-10-05 16:25:31 -0300

    Subindo projeto

---
+++ Alura.WebAPI/Alura.WebAPI.AuthProvider/LoginController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Alura.ListaLeitura.Seguranca;
+using Alura.WebAPI.AuthProvider;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Alura.ListaLeitura.Services
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly SignInManager<Usuario> _signInManager;
+
+        public LoginController(SignInManager<Usuario> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Token(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Realiza a tentativa de login de acordo com o usuário e senha do parâmetro;
+                var result = await _signInManager.PasswordSignInAsync(model.Login, model.Password, true, true);
+                if (result.Succeeded)
+                {
+                    //payload(direitos)
+                    var direitos = new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub, model.Login),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                    };
+
+
+                    //signature (chave + credenciais);
+                    var chave = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("alura-webapi-authentication-valid"));
+                    var credenciais = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+                    var token = new JwtSecurityToken(
+                        issuer: "Alura.WebApp",
+                        audience: "Postman",
+                        claims: direitos,
+                        signingCredentials: credenciais,
+                        expires: DateTime.Now.AddMinutes(30)
+                    );
+
+
+                    //cria o token (header + payload (direitos) + signature (chave + credenciais));
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+                    var logado = new RetornoLogin(model.Login, tokenString, DateTime.Now);
+                    return Ok(logado);
+                }
+                return Unauthorized(); //401
+            }
+            return BadRequest(); //400
+        }
+    }
+}
